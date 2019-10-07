f16d0d3d724d7114ed2b6af393d579cf5603ba96
Net-zen <prankster@myttk.ru>
2019-10-07 10:19:13 +0600

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {this.setRange()}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.try = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.setRange(this.min, this.try);
     }
 
     greater() {
-
+        this.setRange(this.try, this.max);
     }
 }
 
