bbff7513031e1c3fa20ee777b913972135e61561
Net-zen <prankster@myttk.ru>
2019-10-07 11:37:47 +0600

    refactor: rename variable

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -7,15 +7,15 @@ class GuessingGame {
     }
 
     guess() {
-        return this.try = Math.ceil((this.min + this.max) / 2);
+        return this.attempt = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-        this.setRange(this.min, this.try);
+        this.setRange(this.min, this.attempt);
     }
 
     greater() {
-        this.setRange(this.try, this.max);
+        this.setRange(this.attempt, this.max);
     }
 }
 
